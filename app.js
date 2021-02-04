e1c344cdb1aa139a92bdc0685c5bfad7586663d4
Chloe Williams <46544086+chlo3-w@users.noreply.github.com>
2021-02-04 15:31:24 +0000

    new template

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+//Routes
+app.use('/', require('./routes/index'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
+
