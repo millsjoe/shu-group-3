6769dabadfa336293ba84a1088c6da383a9f5a97
Lucy Burgin <lucy.burgin@skybettingandgaming.com>
2021-02-05 14:55:09 +0000

    first try

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 
 //Routes
-app.use('/', require('./routes/index'));
+app.use("/", require("./routes/index.js"));
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
+// unsure how to connect the html to the localhost
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
