cf7a46fd9b8206dd0e94cb709f106a658507009f
FIONA SIMONS <FIONA.SIMONS@sbgml06652.sbg.skybettingandgaming.com>
2021-02-09 14:43:29 +0000

    bcrypt and passport

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session'); 
+const passport = require('passport');
 
 //Creating App
 const app = express();
+
+//Passport config
+require('./config/passport')(passport);
 
 //DB Connection
 const db = require('./config/keys').MongoURI;
@@ -22,6 +28,29 @@ app.set('view engine', 'ejs');
 
 //BodyParser
 app.use(express.urlencoded({ extended: false }))
+
+// Express Session
+app.use(
+    session({
+      secret: 'secret',
+      resave: true,
+      saveUninitialized: true
+    })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Connect flash
+app.use(flash());
+
+//Global Vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    next();
+});
 
 //Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -68,6 +68,7 @@ router.post('/register', (req, res) => {
                         //Save user
                         newUser.save()
                             .then(user => {
+                                req.flash('success_msg', 'You are now registered and can log in');
                                 res.redirect('/login');
                             })
                             .catch(err => console.log(err));
