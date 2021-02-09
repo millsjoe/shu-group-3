29564da986a4e3fb579895224316f6e8f5b279b2
Chloe Williams <46544086+chlo3-w@users.noreply.github.com>
2021-02-09 11:21:38 +0000

    register Users

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://shu-group3:test@cluster0.tqmii.mongodb.net/test?retryWrites=true&w=majority'
+    MongoURI: 'mongodb+srv://shu-group3:test@cluster0.tqmii.mongodb.net/ddsa?retryWrites=true&w=majority'
 }
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+//User model
+const User = require('../models/User');
 
 //Login Page
 router.get('/login', (req, res) => res.render('Login'));
@@ -34,9 +38,42 @@ router.post('/register', (req, res) => {
             email,
             password,
             password2
-        });
+          });
     } else {
-        res.send('pass');
+        //Validation passed
+        User.findOne({ email: email}).then(user => {
+            if(user) {
+                // User exists
+                errors.push({msg: 'Email is already registered '});
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                });
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password
+                });
+                
+                //Hash Password with bcrypt
+                bcrypt.genSalt(10, (err, salt) => 
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        //Set password to hashed password
+                        newUser.password = hash;
+                        //Save user
+                        newUser.save()
+                            .then(user => {
+                                res.redirect('/login');
+                            })
+                            .catch(err => console.log(err));
+                }))
+            }
+        });
     }
 });
 
