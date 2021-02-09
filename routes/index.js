998c44d072d6ec2e21cbf5a51ab944cacf73eb07
Lucy Burgin <lucy.burgin@skybettingandgaming.com>
2021-02-09 15:03:35 +0000

    updates including CSS files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 //code from Chris
 const express = require("express");
 const router = express.Router();
+const bodyParser = require("body-parser");
 
 router.get('/', (req, res) => res.render('index'));
 
@@ -39,68 +40,68 @@ const hashString = function (string) {
   return crypto.createHash("sha256").update(string).digest("hex");
 };
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+// const app = express();
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(
-  session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true,
-  })
-);
+// app.use(
+//   session({
+//     secret: "secret",
+//     resave: true,
+//     saveUninitialized: true,
+//   })
+// );
 
-const port = 3000;
+// const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send('Hello World! Click <a href="/login">Login</a> to Login');
-});
+// app.get("/", (req, res) => {
+//   res.send('Hello World! Click <a href="/login">Login</a> to Login');
+// });
 
-app.get("/login", (req, res) => {
-  res.sendFile("login.html", { root: path.join(__dirname, "public") });
-});
+// app.get("/login", (req, res) => {
+//   res.sendFile("login.html", { root: path.join(__dirname, "public") });
+// });
 
-app.get("/secret", function (request, response) {
-  if (request.session.loggedin) {
-    response.send("Welcome back, " + request.session.username + "!");
-  } else {
-    response.send(
-      'Please login to view this page! Click <a href="/login">Login</a> to Login'
-    );
-  }
-  response.end();
-});
+// app.get("/secret", function (request, response) {
+//   if (request.session.loggedin) {
+//     response.send("Welcome back, " + request.session.username + "!");
+//   } else {
+//     response.send(
+//       'Please login to view this page! Click <a href="/login">Login</a> to Login'
+//     );
+//   }
+//   response.end();
+// });
 
-app.post("/login", (request, response) => {
-  const username = request.body.username;
-  const password = request.body.password;
+// app.post("/login", (request, response) => {
+//   const username = request.body.username;
+//   const password = request.body.password;
 
-  console.log(`The user ${username} is trying to login`);
+//   console.log(`The user ${username} is trying to login`);
 
-  if (doesUserExist(username)) {
-    console.log(`The user ${username} is a valid user`);
+//   if (doesUserExist(username)) {
+//     console.log(`The user ${username} is a valid user`);
 
-    if (checkUserPassword(username, password)) {
-      console.log(`Password correct`);
-      request.session.loggedin = true;
-      request.session.username = username;
-      return response
-        .status(201)
-        .send(
-          'Password Correct. Click <a href="/secret">secret</a> to see the secret content'
-        );
-    } else {
-      console.log(
-        `The user's ${username} is correct, but an invalid password was supplied`
-      );
-      return response.status(403).send("Password incorrect");
-    }
-  } else {
-    console.log(`The user ${username} is an invalid user`);
-    return response.send("User does not exist", 403);
-  }
-});
+//     if (checkUserPassword(username, password)) {
+//       console.log(`Password correct`);
+//       request.session.loggedin = true;
+//       request.session.username = username;
+//       return response
+//         .status(201)
+//         .send(
+//           'Password Correct. Click <a href="/secret">secret</a> to see the secret content'
+//         );
+//     } else {
+//       console.log(
+//         `The user's ${username} is correct, but an invalid password was supplied`
+//       );
+//       return response.status(403).send("Password incorrect");
+//     }
+//   } else {
+//     console.log(`The user ${username} is an invalid user`);
+//     return response.send("User does not exist", 403);
+//   }
+// });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
