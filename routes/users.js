3afb3557f935173e8354fd721b64859008dd6bb3
Lucy Burgin <lucy.burgin@skybettingandgaming.com>
2021-02-10 15:21:43 +0000

    new changes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+//Profile Page
+router.get('/profile', (req, res) => res.render('Profile'));
+
 //Login Page
 router.get('/login', (req, res) => res.render('Login'));
 
