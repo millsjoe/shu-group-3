9fab9a7c169aa3f73ff3eafec82e9be7e676552f
Chloe Williams <46544086+chlo3-w@users.noreply.github.com>
2021-02-12 12:01:33 +0000

    finished ratings and error messages

--- routes/ratings.js
+++ routes/ratings.js
@@ -14,37 +14,45 @@ router.post('/', ensureAuthenticated, async (req, res) => {
     try {
       req.body.user = req.user.id
       await Rating.create(req.body)
+      req.flash('success_msg', 'Rating added');
       res.redirect('/profile')
     } catch (err) {
       console.error(err)
+      res.render('error/500')
     }
   })
 
 // Show edit page
 router.get('/edit/:id', ensureAuthenticated, async (req, res) => {
-  const ratings = await Rating.findOne({
-    _id: req.params.id
-  })
-
-  if (!ratings) {
-    return console.error(err)
-  }
-
-  if (ratings.user != req.user.id) {
-    res.redirect ('/home')
-  } else{
-    res.render('ratings/edit', {
-      ratings,
+  try {
+    const ratings = await Rating.findOne({
+      _id: req.params.id
     })
+  
+    if (!ratings) {
+      return res.render('error/404')
+    }
+  
+    if (ratings.user != req.user.id) {
+      res.redirect ('/home')
+    } else{
+      res.render('ratings/edit', {
+        ratings,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
   }
 })
 
 // Update rating
 router.put ('/:id', ensureAuthenticated, async (req, res) => {
+  try {
   let rating = await Rating.findById(req.params.id)
 
   if (!rating) {
-    return console.log(err)
+    return res.render('error/404')
   }
 
   if (rating.user != req.user.id) {
@@ -54,9 +62,41 @@ router.put ('/:id', ensureAuthenticated, async (req, res) => {
       new: true,
       runValidators: true,
     })
-
-    res.redirect('/home')
+    req.flash('success_msg', 'Rating updated');
+    res.redirect('/profile')
+  }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
   }
 })
+
+//Delete rating
+router.delete('/:id', ensureAuthenticated, async (req, res) => {
+  try {
+    await Rating.remove({_id: req.params.id })
+    req.flash('success_msg', 'Rating deleted');
+    res.redirect('/profile')
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
+})
+
+// CODE FOR LOOKING UP RATINGS AGAINST COFFEE SHOPS
+// router.get('/', ensureAuth, async (req, res) => {
+//   try {
+//     const ratings = await Rating.find({ status: 'public' -- CHANGE CODE })
+//       .populate('user')
+//       .sort({ createdAt: 'desc' })
+
+//     res.render('shops/index', {
+//       ratings,
+//     })
+//   } catch (err) {
+//     console.error(err)
+//     res.render('error/500')
+//   }
+// })
 
 module.exports = router;
